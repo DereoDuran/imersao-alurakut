29a91fd121c5a0c18fff0afba547024557dc9419
José Duran <jose.duran@creditas.com>
2021-07-12 21:49:19 -0300

    first class

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,35 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { AlurakutStyles } from "../src/lib/Commons"
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  /* Reset CSS */
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+  body {
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+  ${AlurakutStyles}
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: "#0070f3",
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +39,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,66 @@
-import styled from 'styled-components'
+import { Container, Box } from "../src/layout/";
+import { MainGrid, ProfileRelationsBoxWrapper } from "../src/components/";
+import { AlurakutMenu, OrkutNostalgicIconSet } from "../src/lib/Commons";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const ProfileSidebar = ({ githubUser }) => {
+  return (
+    <Box>
+      <img src={`https://github.com/${githubUser}.png`} />
+    </Box>
+  );
+};
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = "dereoduran";
+  const people = [
+    "diasvillar",
+    "brunofernandes35",
+    "wboccato",
+    "arturyuiti",
+    "mity-hoshino",
+    "karymereis1",
+    "gkdekker",
+    "diaslaris",
+    "luryrodrigues",
+    "flascardoso",
+    "copeliacoral",
+    "juliana-romero",
+    "igor-araujo",
+    "guferreircreditas",
+  ];
+  const orgs = ["creditas", "google", "facebook", "openai", "deepmind"];
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <Container gridArea="profileArea">
+          <ProfileSidebar {...{ githubUser }} />
+        </Container>
+        <Container gridArea="welcomeArea">
+          <Box>
+            <h2>Bem-vindo(a)</h2>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </Container>
+        <Container gridArea="profileRelationsArea">
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Meus amigos ({people.length})</h2>
+            <ul>
+              {people.map((person) => {
+                return (
+                  <li>
+                  <a href={`https://github.com/${person}`} key={person}>
+                    <img src={`https://github.com/${person}.png`} />
+                    <span>{person}</span>
+                  </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>Comunidades</Box>
+        </Container>
+      </MainGrid>
+    </>
+  );
 }
