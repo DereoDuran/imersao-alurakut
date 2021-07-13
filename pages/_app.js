187d71b7638cf14f1638048c59353ad02a376109
José Duran <jose.duran@creditas.com>
2021-07-12 21:39:25 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,18 @@
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { AlurakutStyles } from "../src/lib/Commons"
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  /* Reset CSS */
-  * {
+  body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-  body {
-    font-family: sans-serif;
-    background-color: #D9E6F6;
-  }
-  #__next {
-    display: flex;
-    min-height: 100vh;
-    flex-direction: column;
-  }
-  img {
-    max-width: 100%;
-    height: auto;
-    display: block;
-  }
-  ${AlurakutStyles}
-`;
+`
 
 const theme = {
   colors: {
-    primary: "#0070f3",
+    primary: '#0070f3',
   },
-};
+}
 
 export default function App({ Component, pageProps }) {
   return (
@@ -39,5 +22,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  );
+  )
 }
--- src/layout/Container.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  @media (min-width: 860px) {
-    ${(props) => props.gridArea && `grid-area: ${props.gridArea}`}
-  }
-`;
