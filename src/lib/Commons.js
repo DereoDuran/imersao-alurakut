3a276f90ab85430c0e03ce46dd74291999a6fc47
José Duran <jose.duran@creditas.com>
2021-07-17 16:32:34 -0300

    fix sidebar logout

--- src/lib/Commons.js
+++ src/lib/Commons.js
@@ -233,7 +233,7 @@ export function AlurakutProfileSidebarMenuDefault() {
           <img src={`${BASE_URL}/icons/plus.svg`} />
           GitHub Trends
         </a>
-        <a href="/logout">
+        <a onClick={logOut}>
           <img src={`${BASE_URL}//icons/logout.svg`} />
           Sair
         </a>
